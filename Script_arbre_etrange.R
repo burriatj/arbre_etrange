8408285186fa7630dfe29e23375f06be1d5c40bb
fanny bourgeau <fanny.bourgeau@ens-lyon.fr>
2024-05-13 14:09:20 +0200

    Ajout de dialogue

--- Script_arbre_etrange.R
+++ Script_arbre_etrange.R
@@ -2,5 +2,6 @@
 
 Il était une fois, dans une forêt lointaine, un arbre étrange...
 De ses branches pendaient des milliers de petites feuilles dont émanait une odeur envoûtante. 
-Quand on y prêtait attention, ces feuilles ressemblaient étrangement à de délicats petits sachets de tisane. 
+Quand on y prêtait attention, ces feuilles ressemblaient étrangement à de délicats petits sachets de tisane.
+Les sachets murmuraient ces paroles : " infuse moiiii... "
 Chacun pouvait se servir après le repas, ou avant d'aller dormir.
