76be1c7a602e1cdd5de88a637b826926e3600039
fanny bourgeau <fanny.bourgeau@ens-lyon.fr>
2024-05-13 11:25:17 +0200

    suite histoire

--- Script_arbre_etrange.R
+++ Script_arbre_etrange.R
@@ -1,3 +1,6 @@
 ## Histoire
 
 Il était une fois, dans une forêt lointaine, un arbre étrange...
+De ses branches pendaient des milliers de petites feuilles dont émanait une odeur envoûtante. 
+Quand on y prêtait attention, ces feuilles ressemblaient étrangement à de délicats petits sachets de tisane. 
+Chacun pouvait se servir après le repas, ou avant d'aller dormir.
