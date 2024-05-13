071c0eca6ed6763b1d44f5ce12205d77b01dd7ce
Josselin <burriatj@gmail.com>
2024-05-13 14:10:09 +0200

    Ajout adjectifs

--- Script_arbre_etrange.R
+++ Script_arbre_etrange.R
@@ -1,6 +1,7 @@
 ## Histoire
 
-Il était une fois, dans une forêt lointaine, un arbre étrange...
-De ses branches pendaient des milliers de petites feuilles dont émanait une odeur envoûtante. 
-Quand on y prêtait attention, ces feuilles ressemblaient étrangement à de délicats petits sachets de tisane. 
-Chacun pouvait se servir après le repas, ou avant d'aller dormir.
+Il était une fois, dans une forêt lointaine, un arbre, somme toute étrange...
+De ses branches pendaient des milliers de petites feuilles surprenantes dont émanait une odeur envoûtante. 
+Quand on y prêtait vraiment attention, ces drôles de feuilles ressemblaient étrangement à de délicats petits sachets de tisane. 
+Chacun pouvait se servir à sa guise après le repas, ou avant d'aller dormir paisiblement.
+
